2acbd1ba544a9e26b3c6b366065232251098d5af
Marudhuk <marudhu2001@gmail.com>
2023-04-28 23:55:03 +0530

    api fetching in js and html

--- weather.js
+++ weather.js
@@ -1,9 +1,3 @@
-// const options = {method: 'GET', headers: {accept: 'application/json'}};
-
-// fetch('https://api.tomorrow.io/v4/weather/realtime?location=toronto&apikey=TFUTuloRyqD1vTtg6kxPOsucf6YCkbFm', options)
-//   .then(response => response.json())
-//   .then(response => console.log(response))
-//   .catch(err => console.error(err));
 
 
-fetch('https://api.tomorrow.io/v4/weather/realtime?location=toronto&apikey=TFUTuloRyqD1vTtg6kxPOsucf6YCkbFm').then(res => res.json()).then(res1 => console.log(res1))
+fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(res1 => console.log(res1))
