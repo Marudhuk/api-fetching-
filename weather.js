1e2c384eed36aa5628749accfcf6a08069168eac
Marudhuk <marudhu2001@gmail.com>
2023-05-03 14:14:34 +0530

    adding card to show data from api

--- weather.js
+++ weather.js
@@ -1,3 +1,5 @@
-
-
-fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(res1 => console.log(res1))
+fetch('http://api.weatherstack.com/current?access_key=76e3258675ed6b7fd36501613d41a09b&query=New York')
+.then(res => res.json())
+.then(users => {
+    console.log(users)
+})
